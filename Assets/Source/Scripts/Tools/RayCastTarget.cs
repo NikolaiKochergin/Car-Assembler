e45e5efe472debfbcc90fc6f86ed3a9e42cf66ce
Nikolai Kochergin <Kocherginizer@gmail.com>
2022-10-13 11:21:28 +0300

    Added RayCastTarget

---
+++ Assets/Source/Scripts/Tools/RayCastTarget.cs
@@ -0,0 +1,10 @@
+using UnityEngine.UI;
+
+namespace CarAssembler
+{
+    public class RayCastTarget : Graphic
+    {
+        public override void SetMaterialDirty() { return; }
+        public override void SetVerticesDirty() { return; }
+    }
+}
