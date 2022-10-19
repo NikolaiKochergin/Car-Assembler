75b220306a554917432844e096b48e98916e0971
Nikolai Kochergin <Kocherginizer@gmail.com>
2022-10-19 15:04:09 +0300

    IconsMap refactoring

--- Assets/Source/Scripts/UI/UI.cs
+++ Assets/Source/Scripts/UI/UI.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace CarAssembler
@@ -8,10 +10,36 @@ namespace CarAssembler
         [SerializeField] private PlayMenu _playMenu;
         [SerializeField] private FinisherMenu _finisherMenu;
         [SerializeField] private EndLevelMenu _endLevelMenu;
+        
+        [Header("Task Icons")] 
+        
+        [SerializeField] private Sprite _speedIcon;
+        [SerializeField] private Sprite _fuelEconomyIcon;
+        [SerializeField] private Sprite _powerIcon;
+        [SerializeField] private Sprite _coolnessIcon;
+        [SerializeField] private Sprite _comfortIcon;
+        
+        private Dictionary<FeatureType, Sprite> _iconsMap = new();
 
         public MainMenu MainMenu => _mainMenu;
         public PlayMenu PlayMenu => _playMenu;
         public FinisherMenu FinisherMenu => _finisherMenu;
         public EndLevelMenu EndLevelMenu => _endLevelMenu;
+
+        public IReadOnlyDictionary<FeatureType, Sprite> IconsMap => _iconsMap;
+
+        private void Awake()
+        {
+            InitIconsMap();
+        }
+
+        private void InitIconsMap()
+        {
+            _iconsMap.Add(FeatureType.Speed, _speedIcon);
+            _iconsMap.Add(FeatureType.FuelEconomy, _fuelEconomyIcon);
+            _iconsMap.Add(FeatureType.Power, _powerIcon);
+            _iconsMap.Add(FeatureType.Coolness, _coolnessIcon);
+            _iconsMap.Add(FeatureType.Comfort, _comfortIcon);
+        }
     }
 }
--- Assets/Source/Scripts/UI/UITaskListWidget.cs
+++ Assets/Source/Scripts/UI/UITaskListWidget.cs
@@ -8,22 +8,13 @@ namespace CarAssembler
         [SerializeField] private Transform _content;
         [SerializeField] private UITaskWidget _taskWidgetPrefab;
 
-        [Header("Task Icons")] [SerializeField]
-        private Sprite _speedIcon;
-
-        [SerializeField] private Sprite _fuelEconomyIcon;
-        [SerializeField] private Sprite _powerIcon;
-        [SerializeField] private Sprite _coolnessIcon;
-        [SerializeField] private Sprite _comfortIcon;
-
         private readonly List<UITaskWidget> _taskWidgets = new();
         private CarFeatures _carFeatures;
 
-        public Dictionary<FeatureType, Sprite> IconMap { get; } = new();
+        private IReadOnlyDictionary<FeatureType, Sprite> _iconsMap;
 
         private void Awake()
         {
-            InitIconsMap();
             Hide();
         }
 
@@ -33,27 +24,20 @@ namespace CarAssembler
                 _carFeatures.CarFeaturesChanged -= OnCarFeaturesChanged;
         }
 
-        public void Initialize(IReadOnlyList<Task> tasks, CarFeatures carFeatures)
+        public void Initialize(IReadOnlyList<Task> tasks, CarFeatures carFeatures, IReadOnlyDictionary<FeatureType, Sprite> iconsMap)
         {
+            _iconsMap = iconsMap;
+            
             foreach (var task in tasks)
             {
                 var spawnedWidget = Instantiate(_taskWidgetPrefab, _content);
-                spawnedWidget.Initialize(IconMap[task.FeatureType], task.FeatureType);
+                spawnedWidget.Initialize(_iconsMap[task.FeatureType], task.FeatureType);
                 _taskWidgets.Add(spawnedWidget);
             }
 
             _carFeatures = carFeatures;
             _carFeatures.CarFeaturesChanged += OnCarFeaturesChanged;
             OnCarFeaturesChanged(_carFeatures);
-        }
-
-        private void InitIconsMap()
-        {
-            IconMap.Add(FeatureType.Speed, _speedIcon);
-            IconMap.Add(FeatureType.FuelEconomy, _fuelEconomyIcon);
-            IconMap.Add(FeatureType.Power, _powerIcon);
-            IconMap.Add(FeatureType.Coolness, _coolnessIcon);
-            IconMap.Add(FeatureType.Comfort, _comfortIcon);
         }
 
         private void OnCarFeaturesChanged(CarFeatures carFeatures)
