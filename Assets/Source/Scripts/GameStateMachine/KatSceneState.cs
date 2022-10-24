cca37024af83172114a621ae1bdff724c92897a8
Nikolai Kochergin <Kocherginizer@gmail.com>
2022-10-24 15:35:49 +0300

    Change enter kat scene

--- Assets/Source/Scripts/GameStateMachine/KatSceneState.cs
+++ Assets/Source/Scripts/GameStateMachine/KatSceneState.cs
@@ -5,23 +5,37 @@ namespace CarAssembler
     public class KatSceneState : IGameState
     {
         private readonly PlayableDirector _enterKatScene;
+        private readonly GameStateMachine _gameStateMachine;
         private readonly UI _ui;
-        
-        public KatSceneState (PlayableDirector director, UI ui)
+        private readonly MainCameraContainer _mainCameraContainer;
+
+        public KatSceneState(GameStateMachine gameStateMachine, PlayableDirector director, UI ui, MainCameraContainer mainCameraContainer)
         {
+            _gameStateMachine = gameStateMachine;
             _enterKatScene = director;
             _ui = ui;
+            _mainCameraContainer = mainCameraContainer;
         }
 
         public void Enter()
         {
-            _ui.PlayMenu.Show();
             _enterKatScene.Play();
+            _ui.MainMenu.StartButton.ButtonPressedDown += OnButtonPressedDown;
         }
 
         public void Exit()
         {
             _enterKatScene.Stop();
+            _ui.MainMenu.StartButton.ButtonPressedDown += OnButtonPressedDown;
+        }
+
+
+        private void OnButtonPressedDown()
+        {
+            _mainCameraContainer.CameraAnimator.ShowMoveToPlayStateAnimation();
+            _ui.PlayMenu.CustomerCloud.gameObject.SetActive(false);
+            _ui.MainMenu.Hide();
+            _gameStateMachine.SetPlayState();
         }
     }
 }
