7f20024cfbb06af7f717f738431bd0d3b90e5935
Maxim Chibisov <maxdeveloper312@gmail.com>
2022-10-27 22:13:55 +0300

    Add barrier.

--- Assets/Source/Scripts/Obstacles/Barrier.cs
+++ Assets/Source/Scripts/Obstacles/Barrier.cs
@@ -8,10 +8,12 @@ namespace CarAssembler
         [SerializeField] private Detail _penaltyDetailPrefab;
         [SerializeField] [Min(0)] private float _moveSpeed;
         [SerializeField] [Min(0)] private float _moveDuration;
+        [SerializeField] private ContainerAnimator _containerAnimator;
 
         public Detail PenaltyDetail => _penaltyDetailPrefab;
         public float MoveSpeed => _moveSpeed;
         public float MoveDuration => _moveDuration;
+        public ContainerAnimator ContainerAnimator => _containerAnimator;
 
         public void Disable()
         {
