869859b5d17d31e3461b8108f8084d2a56dc1158
Maxim Chibisov <maxdeveloper312@gmail.com>
2022-10-28 16:21:49 +0300

    finisher

--- Assets/Source/Animations/MainCamera/MainCameraAnimator.cs
+++ Assets/Source/Animations/MainCamera/MainCameraAnimator.cs
@@ -5,12 +5,18 @@ namespace CarAssembler
     public class MainCameraAnimator : MonoBehaviour
     {
         private const string MoveToPlayStateAnimation = nameof(MoveToPlayStateAnimation);
+        private const string CameraRide = nameof(CameraRide);
         
         [SerializeField] private Animator _animator;
 
         public void ShowMoveToPlayStateAnimation()
         {
             _animator.Play(MoveToPlayStateAnimation);
+        }
+        
+        public void ShowMoveToFinishStateAnimation()
+        {
+            _animator.Play(CameraRide);
         }
     }
 }
