44b825e461448e2f724653867df81755569eb6f5
Nikolai Kochergin <Kocherginizer@gmail.com>
2022-11-03 11:56:24 +0300

    Add lock Y to camera folower

--- Assets/Source/Scripts/Camera/Follower.cs
+++ Assets/Source/Scripts/Camera/Follower.cs
@@ -7,11 +7,18 @@ namespace CarAssembler
         [SerializeField] private Transform _target;
         [SerializeField] [Min(0)] private float _followSpeed;
         [SerializeField] [Min(0)] private float _rotateSpeed;
+        [SerializeField] private bool _lockYAxisMovement;
 
         private void Update()
         {
-            transform.position =
+            var currentPosition =
                 Vector3.Lerp(transform.position, _target.transform.position, _followSpeed * Time.deltaTime);
+
+            if (_lockYAxisMovement)
+                currentPosition = new Vector3(currentPosition.x, 0, currentPosition.z);
+            
+            transform.position = currentPosition;
+            
             transform.rotation = Quaternion.Lerp(transform.rotation, _target.transform.rotation,
                 _rotateSpeed * Time.deltaTime);
         }
